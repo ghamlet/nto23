79be158d797aa0b2d154a0cf647ecc4b82f50c9f
ghamlet <dmtr317744@gmail.com>
2023-12-11 18:57:08 +0300

    object tracking

--- second_stage/1_task/YOLO_test/yolo_beyondrobot.py
+++ second_stage/1_task/YOLO_test/yolo_beyondrobot.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
-import time
-import sys
+import math
+
 import os
 
 Conf_threshold = 0.1
@@ -10,11 +10,11 @@ COLORS = [(0,255,0),(0,0,255),(255,0,0),(225,255,0),(255,0,255),(0,255,255)]
 
 class_name = []
 
-with open("second_stage/1_task/YOLO_test/object-detection/data/coco.names") as f:
+with open("second_stage/1_task/YOLO_test/object-detection/data/coco.names") as f: 
     class_name = [cname.strip() for cname in f.readlines()]
 
-config_path = "second_stage/1_task/YOLO_test/object-detection/cfg/yolov4-obj _2.cfg"  # конфигурация нейронной сети
-weights_path = "second_stage/1_task/YOLO_test/object-detection/data/yolov4-obj_best.weights"  # файл весов сети YOLO
+config_path = "d:/weights/Yolov4/yolov4-obj.cfg"  # конфигурация нейронной сети
+weights_path = "d:/weights/Yolov4/yolov4-obj_best.weights "
 
 
 # загружаем сеть YOLO
@@ -26,40 +26,72 @@ model = cv2.dnn.DetectionModel(net)
 model.setInputParams(size= (608,608), scale= 1/255, swapRB=True)
 
 
-files = os.listdir("C:/Users/max30/Desktop/videos/")
+files = os.listdir("d:/videos")
 
+counter = 0
+center_points_prev_frame = []
+tracking_objects = {}
+track_id = 0
 
 for pos in range(len(files)):
-    cap = cv2.VideoCapture("C:/Users/max30/Desktop/videos/" + files[pos])
-    starting_time = time.time()
-    frame_counter = 0
+    cap = cv2.VideoCapture("d:/videos/" + files[pos])
+    
 
     while True:
         ret, frame = cap.read()
-        frame_counter+=1
-
+        counter+=1
         if not ret:
             break
 
+        center_points_cur_frame = []
+
         classes, scores, boxes = model.detect(frame, Conf_threshold, NMS_threshold)
-        for (classid, score, box) in zip(classes, scores, boxes):
-            color = COLORS[int(classid) % len(COLORS)]
+      
+        for box in boxes:
+            (x,y,w,h) = box
+            cx = int((x + x + w) / 2)
+            cy = int((y + y + h) / 2)
+            center_points_cur_frame.append((cx, cy))
 
-            label =  (class_name[classid], score)
 
-            cv2.rectangle(frame, box, color, 1)
-            cv2.putText(frame, str(label), (box[0], box[1]-10), cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 1)
+        
+        if counter <= 2:
+            for pt in center_points_cur_frame:
+                for pt2 in center_points_prev_frame:
+                    distance = math.hypot(pt2[0] - pt[0], pt2[1]  -pt[1])
+                    print(distance)
+
+                    if distance < 150:
+                        tracking_objects[track_id] = pt
+                        track_id +=1
+        else:
+            for pt in center_points_cur_frame:
+                for object_id, pt2 in tracking_objects.items():
+                    distance = math.hypot(pt2[0] - pt[0], pt2[1]  -pt[1])
+                    if distance < 150:
+                        tracking_objects[object_id] = pt
 
 
-        endingTime = time.time() - starting_time
-        fps = round(frame_counter / endingTime)
-        cv2.putText(frame, f"FPS: {fps}", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+        for object_id, pt in tracking_objects.items():
+            cv2.circle(frame, pt, 5, (255,0,255), -1)
+            cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0,1, (255,0,255), 2)
+
+
+            
+
+        print(tracking_objects)
+        center_points_prev_frame = center_points_cur_frame.copy()
+
+
+
+           
 
         cv2.imshow("frame", frame)
 
         key = cv2.waitKey(1)
         if key == ord("q"):
-            break
+            exit(0)
+
         elif key == ord("d"):
             pos+=1
             break
